8d6801be761ab20e5710363768749466dc129f0f
Michael Rawson <michael@rawsons.uk>
2022-01-31 16:03:20 +0000

    usability improvements

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,50 @@ mod unify;
 mod util;
 
 use clap::Parser;
+use std::io::{stdout, Write};
 use std::path::PathBuf;
+use std::process::exit;
+use std::thread;
+use std::time::Duration;
 
 #[derive(Parser)]
 #[clap(author, version, about)]
 struct Options {
     path: PathBuf,
+    #[clap(short, long, default_value_t = 10)]
+    time: u64,
+}
+
+impl Options {
+    fn problem_name(&self) -> &str {
+        self.path
+            .file_stem()
+            .and_then(|stem| stem.to_str())
+            .unwrap_or_default()
+    }
+
+    fn launch_timeout_thread(&self) {
+        let time = self.time;
+        let name = self.problem_name().to_owned();
+        thread::spawn(move || {
+            thread::sleep(Duration::from_secs(time));
+            let stdout = stdout();
+            let mut lock = stdout.lock();
+            writeln!(lock, "% SZS status TimeOut for {}", name)
+                .expect("failed to print timeout message");
+            std::mem::forget(lock);
+            exit(1);
+        });
+    }
 }
 
 fn main() -> anyhow::Result<()> {
     let opts = Options::parse();
+    opts.launch_timeout_thread();
+
     let matrix = tptp::load(&opts.path)?;
-    search::go(&matrix);
+    if search::go(&matrix) {
+        println!("% SZS status Theorem for {}", opts.problem_name());
+    }
     Ok(())
 }
--- src/search.rs
+++ src/search.rs
@@ -16,13 +16,13 @@ impl<'matrix> Search<'matrix> {
         Self { matrix }
     }
 
-    fn go(&mut self) {
+    fn go(&mut self) -> bool {
         for limit in 0.. {
             if self.start(limit) {
-                println!("proved");
-                break;
+                return true;
             }
         }
+        false
     }
 
     fn start(&mut self, limit: usize) -> bool {
@@ -218,59 +218,9 @@ impl<'matrix> Search<'matrix> {
         }
         splits.into_iter().map(Rc::new).collect()
     }
-
-    /*
-    fn possible_rules(&self, matrix: &Matrix) -> Vec<Rule> {
-        let mut result = vec![];
-
-        let goal = self.path.last().expect("no goal");
-        let goal_literal = goal.literals.last().expect("no goal literal");
-
-        result
-    }
-
-    fn compute_splits(&self, matrix: &Matrix, rule: Rule) -> Vec<Arc<Split>> {
-        match rule {
-            Rule::InputResolution {
-                clause,
-                split,
-                literal,
-            } => {
-                let goal = self.path.last().expect("no goal");
-                let clause = &matrix.clauses[clause].0;
-                let mut clause = clause.splits.clone();
-                let goal_literal = goal.literals.last().expect("no goal literal");
-                let mut split = clause.remove(split);
-                let split = Arc::make_mut(&mut split);
-                split.offset(goal.variables);
-                let literal = split.literals.remove(literal);
-
-                let variables = goal.variables + split.variables;
-                let mut unifier = Unifier::new(variables);
-                unifier.unify_fast(literal.atom.as_slice(), goal_literal.atom.as_slice());
-                let literals = goal.literals[..goal.literals.len() - 1]
-                    .iter()
-                    .chain(split.literals.iter())
-                    .map(|literal| Literal {
-                        polarity: literal.polarity,
-                        atom: unifier.apply(literal.atom.as_slice()),
-                    })
-                    .collect();
-                let mut splitter = Splitter::default();
-                let mut splits = splitter.split(literals, variables);
-                let mut renaming = Renaming::default();
-                for split in &mut splits {
-                    split.normalise(&mut renaming);
-                }
-                clause.extend(splits.into_iter().map(Arc::new));
-                clause
-            }
-        }
-    }
-    */
 }
 
-pub(crate) fn go(matrix: &Matrix) {
+pub(crate) fn go(matrix: &Matrix) -> bool {
     let mut search = Search::new(matrix);
-    search.go();
+    search.go()
 }
